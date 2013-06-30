13ebc05a3f975c60ab5ec60e402e0514338f28b1
Xianjing Zhuo <zhuoxianjing@gmail.com>
2013-07-01 01:32:09 +0800

    add date directive

--- app/js/directives.js
+++ app/js/directives.js
@@ -6,4 +6,28 @@ angular.module('myApp.directives', ['myApp.services'])
         return function(scope, element, attr){
             element.text(app.version);
         };
-    }]);
+    }])
+    .directive('date',function(){
+        return {
+            replace: true,
+            template: '<input type="text"/>',
+            restrict: 'E',
+            require: 'ngModel',
+            link: function (scope, element, attr, ngModel) {
+                var currentFormat = "dd-mm-yy";
+                element.bind('change input keyup', function(){
+                    scope.$apply(setModelValue);
+                });
+
+                function setModelValue(){
+                    ngModel.$setViewValue(element.val());
+                }
+                setModelValue();
+
+                ngModel.$parsers.push(function(value){
+                    var isValid = value.match(/[0-9]{2}-[0-9]{2}-[0-9]{2}$/);
+                    ngModel.$setValidity('date',isValid);
+                });
+            }
+        };
+    });
