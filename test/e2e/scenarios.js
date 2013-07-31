e28e70e0c224832bf7dfb0cfa50e3682fe66cdc8
Xianjing Zhuo <zhuoxianjing@gmail.com>
2013-08-01 01:30:00 +0800

    e2e test

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -14,4 +14,17 @@ describe('my app', function() {
   });
 
 
+  describe('view1', function() {
+
+    beforeEach(function() {
+      browser().navigateTo('#/view1');
+    });
+
+
+    it('should render view1 when user navigates to /view1', function() {
+      expect(element('span:first').text()).
+        toMatch(/1.0.0/);
+    });
+
+  });
 });
