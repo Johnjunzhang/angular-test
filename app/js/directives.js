993d4125ed1e5ef963988794a191e14467b83194
Xianjing Zhuo <zhuoxianjing@gmail.com>
2013-08-01 00:41:26 +0800

    add simple display directive

--- app/js/directives.js
+++ app/js/directives.js
@@ -2,27 +2,8 @@
 
 
 angular.module('myApp.directives', [])
-    .directive('date',function(){
-       return {
-           replace: true,
-           template: '<input type="text"/>',
-           restrict: 'E',
-           require: 'ngModel',
-           link: function (scope, element, attr, ngModel) {
-               var currentFormat = "dd-mm-yy";
-               element.bind('change input keyup', function(){
-                   scope.$apply(setModelValue);
-               });
-
-               function setModelValue(){
-                   ngModel.$setViewValue(element.val());
-               }
-               setModelValue();
-
-               ngModel.$parsers.push(function(value){
-                   var isValid = value.match(/[0-9]{2}-[0-9]{2}-[0-9]{2}$/);
-                   ngModel.$setValidity('date',isValid);
-               });
-           }
-       };
+    .directive('appVersion', function(){
+      return function(scope, element, attr){
+        element.text('1.0.0');
+      };
     });
