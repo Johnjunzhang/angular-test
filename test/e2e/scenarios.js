8abdb23c824f2a42e9b9e0cd0ec42b64ed5c0f31
Xianjing Zhuo <zhuoxianjing@gmail.com>
2013-08-01 01:25:10 +0800

    e2e test

---
+++ test/e2e/scenarios.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
+
+describe('my app', function() {
+
+  beforeEach(function() {
+    browser().navigateTo('../../app/index.html');
+  });
+
+
+  it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
+    expect(browser().location().url()).toBe("/view1");
+  });
+
+
+});
