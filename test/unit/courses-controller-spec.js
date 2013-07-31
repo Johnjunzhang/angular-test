de5841a75f47100bb3b58268bf5e2edc5ea79c26
Xianjing Zhuo <zhuoxianjing@gmail.com>
2013-07-31 08:40:27 +0800

    clean test

--- test/unit/courses-controller-spec.js
+++ test/unit/courses-controller-spec.js
@@ -10,7 +10,7 @@ describe("online courses", function(){
     }));
 
     it("should load current courses when initialize controller", function(){
-        expect(scope.currentCourses.length).toBe(2);
+        expect(scope.currentCourses.length).toBe(1);
         expect(scope.currentCourses[0].name).toBe('javascript')
         expect(scope.currentCourses[0].online).toBe('2013/07/30')
     });
