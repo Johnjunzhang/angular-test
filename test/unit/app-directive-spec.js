4d824959cef6bbc2ff4daef8c9a14e05b167464c
xianjing <zhuoxianjing@gmail.com>
2013-08-01 14:36:52 +0800

    correct service spec

--- test/unit/app-directive-spec.js
+++ test/unit/app-directive-spec.js
@@ -4,19 +4,20 @@ describe('app-version', function(){
 
     beforeEach(module('myApp.directives'));
 
-    it("should show version", inject(function($compile,$rootScope){
-        var dateTemplate = '<span app-version></span>"/>';
+    it("should show version", inject(function($compile,$rootScope, $httpBackend){
+        $httpBackend.whenGET(/app/).respond({version:'TEST_VER'});
+        var dateTemplate = '<span app-version></span>';
         var element = $compile(dateTemplate)($rootScope);
-
-        expect(element.text()).toEqual('1.0.0');
+        $httpBackend.flush();
+        expect(element.text()).toEqual('TEST_VER');
     }));
 
     it("should show version", function(){
         module(function($provide){
-            $provide.factory('app',function(){return {version:'Test_Version'}});
+            $provide.factory('app',function(){return {get:function(){return {then: function(callBack){callBack({data:{version:'Test_Version'}})}}}}});
         });
         inject(function($compile,$rootScope){
-            var dateTemplate = '<span app-version></span>"/>';
+            var dateTemplate = '<span app-version></span>';
             var element = $compile(dateTemplate)($rootScope);
 
             expect(element.text()).toEqual('Test_Version');
