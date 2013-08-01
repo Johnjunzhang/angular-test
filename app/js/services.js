369deb50be42d29c866d97ffb8a65e7063d81234
Xianjing Zhuo <zhuoxianjing@gmail.com>
2013-08-01 13:52:38 +0800

    remove unessary success

--- app/js/services.js
+++ app/js/services.js
@@ -4,10 +4,7 @@ angular.module('myApp.services', [])
     .factory('app', ['$http', function ($http) {
         return {
             get: function () {
-                return $http.get('data/app.json').success(function (data) {
-                    return data;
-                }).error(function (data, status) {
-                    });
+                return $http.get('data/app.json');
             }
         }
     }]);
