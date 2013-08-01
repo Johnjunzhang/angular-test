f2ceebcbb0cdd17674e1db71422483dcdab672e3
Xianjing Zhuo <zhuoxianjing@gmail.com>
2013-08-01 12:45:30 +0800

    add test for  and directive which depends on service

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -5,6 +5,7 @@ frameworks = ["jasmine"];
 files = [
   JASMINE,
   JASMINE_ADAPTER,
+  'test/lib/jquery-1.9.1.js',
   'app/lib/angular/angular.js',
   'app/js/**/*.js',
   'test/lib/angular/angular-mocks.js',
--- test/unit/date-directive-spec.js
+++ test/unit/date-directive-spec.js
@@ -4,11 +4,13 @@ describe('date', function(){
 
     beforeEach(module('myApp.directives'));
 
-    xit("should show version", inject(function($compile,$rootScope){
+    it("should show version", inject(function($compile,$rootScope){
         var dateTemplate = '<date ng-model="newAttendee.date"/>';
         var element = $compile(dateTemplate)($rootScope);
-        //jasmine.
-        expect(element.val()).toEqual('11-11-11');
+
+        $(element).val('11-11-11');
+        $(element).trigger('input');
+
         expect($rootScope.newAttendee.date).toEqual('11-11-11');
     }));
 });
